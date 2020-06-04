5247aa82f4c8bb00f552e9f4a6c3e24e5e4868b2
Ramazan BOZKURT <ramazanbozkurt-20@hotmail.com>
2020-06-04 22:07:48 +0300

    -- yeni hatalar belirlendi ve TODO lar yazıldı

--- Gorsel/main.cpp
+++ Gorsel/main.cpp
@@ -8,4 +8,18 @@ int main(int argc, char *argv[])
     Ana_Pencere w;
     w.show();
     return a.exec();
+
+    //TODO sınıf tanımlama kısmında öğretmen komboboxu çalışmıyor.
+
+    //TODO ders ekle kısmı çalışmıyor.
+
+    //TODO öğrenci listesi kısmında düzenleme yapılabiliyor , yapılamaması lazım.
+
+    //TODO öğrenci listesinde çık butonu çalışmıyor.
+
+    //TODO öğrenci eklenemiyor.
+
+    //TODO öğretmen eklendiği zaman 2 tane ekleniyor.
+
+    //TODO öğrenci ekleme ekranının adı değişecek.
 }
